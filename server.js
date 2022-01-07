471418d74012b6d68c8a6d033a61b021b595d45b
Safa Amasha <safaamasha99@gmail.com>
2022-01-07 23:55:52 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,43 @@
+const { ApolloServer, PubSub } = require('apollo-server');
+require('dotenv').config();
+const mongoose = require('mongoose');
+
+// const typeDefs = require('./graphql/typeDefs');
+// const resolvers = require('./graphql/resolvers');
+
+const gql = require('graphql-tag')
+// const pubsub = new PubSub();
+
+const PORT = process.env.port || 5000;
+
+const typeDefs = gql`
+    type Query{
+        sayHi: String!
+    }
+`
+const resolvers = {
+    Query: {
+        sayHi: () => 'Hello World'
+    }
+}
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+
+});
+
+
+mongoose.connect(process.env.DB_URI, {
+    dbName: 'COVID-API',
+    user: process.env.DB_USER,
+    pass: process.env.DB_PASS,
+    useNewUrlParser: true,
+}).then(() => {
+    console.log('MongoDB Connected');
+    return server.listen(PORT);
+}).then((res) => {
+    console.log(`Server running at ${res.url}`);
+}).catch(err => {
+    console.error(err)
+})
